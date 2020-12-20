80be48d3d6cbf311efd647c14240ee043332df07
Mohidul Abedin <mobedin00@gmail.com>
2020-12-20 17:49:01 -0500

    removed images and fixed rendering template

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request, redirect,flash, url_for
 import base64
 import pickle
+import os
 from email.mime.text import MIMEText
-import time
 from googleapiclient.discovery import build
 
 
 
 app = Flask(__name__) #create new instance of flask
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-with open('token.pickle', 'rb') as token:
+DIR = os.path.dirname(__file__) or "."
+DIR += "/"
+with open(DIR+'token.pickle', 'rb') as token:
     creds = pickle.load(token)
 service = build('gmail', 'v1', credentials=creds)
 @app.route("/")
@@ -27,18 +28,18 @@ def create_message(sender, to, subject, message_text):
 @app.route('/email', methods=['POST'])
 def email():
     message = request.form["message"]
-    subject = "[PORTFOLIO] Inquiry from " + request.form["name"]
     email = request.form["email"]
+    subject = "[PORTFOLIO] Inquiry from " + request.form["name"] +" " + email
     if (request.form["name"] == "" or email == ""):
         flash('Please enter a name and email', 'danger')
-        return redirect(url_for('root'))
+        return render_template("index.html")
     if (message == ""):
         flash('Message cannot be empty', 'warning')
     else:
         message = create_message(email,"moabedin00@gmail.com",subject,message)
         service.users().messages().send(userId="me", body=message).execute()
         flash('Message Sent!', 'success')
-    return redirect(url_for('root'))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
